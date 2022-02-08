45ee6941956f4b10e7c9acd492de5b00e843765f
Itchie1411 <richardglennon97@gmail.com>
2022-02-08 21:03:45 +0000

    updating HTML with the function onClick for the player-section buttons to use as a function inside JS. JS has been up and made easily more readable than previous. On-going bugs to compare user and PC selection. At the moment, PC is forced to pick 1 selection, when the user picks the same selection, text appearing show a Draw  is supposed to show in the console log, however this is not happening...yet

--- assests/scripts/scripts.js
+++ assests/scripts/scripts.js
@@ -1,102 +1,33 @@
-// // Wait for DOM to load before running game
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     let buttons = document.getElementsByTagName('button');
-
-//     for(let button of buttons){
-//         button.addEventListener('click', function(){
-//             if(this.getAttribute('data-type') === 'submit'){
-//                 alert('you clicked Submit!');
-//             } else {
-//                 let gameType = this.getAttribute('data-type');
-//                 alert(`you click ${gameType}`);
-//             }
-//         })
-//     }
-// })
-
-// RUN GAME
-// function runGame(){
-
-// }
-
-// //mouse over highlight
-// function highlight() {
-
-// }
-
-// // countdown (after user selects option, count
-// // down from 3)
-// function countdown(){
-
-// }
-
-// //COMPUTERS CHOICE
-//  function computerChoice(){
-
-//  }
-
-// // scores
-// function scores {
-
-// }
-
-// // rest scores and game
-// function reset(){
-
-// }
+//Finding players input
+const playerOption = function playerOption(clicked_id) {
+    playerChoice = clicked_id
+    console.log(playerChoice);
+    
+};
 
 
-const game = () => {
-    //player scores 
-    let playerscore = 0;
-    let pcscore = 0;
+// Getting a random selection of options by the PC:
+computerChoice = addEventListener('click', computerChoice);
+// const pcOptions = ['spock', 'rock', 'paper', 'lizard', 'sci'];
+const pcOptions = ['spock'];
 
-    const playerOption = () => {
-        //finding the selection of buttons
-        // User choice
-        const playerChoice = addEventListener('click', function () {
-        const findingbtns = document.getElementById('player-spock');
-
-
-
-        
-       });
-
-
-        // PC choice
-        const pcOptions = ['spock', 'rock', 'paper', 'lizard', 'sci'];
-        
-        const computerChoice = addEventListener('click', function () {
-
-            const pcNumber = Math.floor(Math.random() * 6);
-            const pcChoice = pcOptions[pcNumber]
-            //console logging PCs choice
-            console.log(`PC picked ${pcChoice}`)
-        });
-    }
-
-    // const winners = (playerSelection, pcChoice) => {
-        
-    //     if(playerSelection === 'paper'){
-    //         if(pcChoice === 'rock'){
-    //             console.log('player wins!')
-    //             playerscore++;
-    //             return;
-    //         } else {
-    //             console.log('PC wins!')
-    //             pcscore++;
-    //             return;
-    //     } 
-
-    //     }
-    // }
-
-
-//call inner functions here
-playerOption()
-// winners()
+function computerChoice() {
+    const pcNumber = Math.floor(Math.random());
+    const pcChoice = pcOptions[pcNumber]
+    // Console out PC choice
+    console.log(pcChoice)
+    return;
 }
 
-//call main function here
-game()
+
+//adding winning conditions
+function winners (playerOption, computerChoice) {
+    if(playerOption == computerChoice) {
+            console.log('draw!');
+            return;
+        }
+
+}
+playerOption();
+
+winners();
