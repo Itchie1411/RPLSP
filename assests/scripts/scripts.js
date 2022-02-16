241053f7fce42c17c380728f1e5fb352a76a4fae
Itchie1411 <richardglennon97@gmail.com>
2022-02-16 11:20:17 +0000

    adding images folder and new JS code. Code is all based in 1 function named Game. The fucntion checks for player button selection, then outputs a random PC choice selection of the same buttons and the newly added winning conditions check the player and PC choices to determine the score

--- assests/scripts/scripts.js
+++ assests/scripts/scripts.js
@@ -1,18 +1,40 @@
-// Finding buttons for player
+// Finding buttons
 const playerOptions = document.querySelectorAll('.player-gamebtn');
-// PC buttons
-const gameOptions = document.querySelectorAll('.gamebtn');
+// // Loop to get all of the player button classes and find the selection with when clicked
 
-// Loop to get all of the player button classes and find the selection with when clicked
-for (let i = 0; i < playerOptions.length; i++) {
-  playerOptions[i].addEventListener('click', function () {
-    console.log(this.innerText);
 
-    // Getting a random selection by the PC:
-    // PC will pci k1
-    const pcChoice = Math.floor(Math.random() * gameOptions.length)
-    let pcPick = pcChoice.toString();
-    console.log(pcPick);
-  });
+function game() {
+  // getting the player button input and choice
+  for (let i = 0; i < playerOptions.length; i++) {
+    playerOptions[i].addEventListener('click', function () {
+      const playerPick = this.innerText;
+      console.log(playerPick);
 
+      ///PC choice
+      const pcPick = document.getElementsByClassName('player-gamebtn');
+      const pcOption = pcPick[Math.floor(Math.random() * pcPick.length)];
+      console.log(pcOption.textContent)
+
+
+      // winning conditions 
+      if (playerPick === pcOption.textContent) {
+        console.log('draw')
+        return;
+      }
+      if (playerPick === 'sci') {
+        if (pcOption.textContent === 'paper')
+          console.log('You win!')
+          return;
+      } else {
+        console.log('PC won..')
+        return;
+      }
+    })
+  }
 };
+
+// Printing the choices to the console. 
+function choices() {
+  console.log(game())
+}
+choices()
