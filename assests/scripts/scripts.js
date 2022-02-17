197175a44e0350592daac61444f27c42a2580467
Itchie1411 <richardglennon97@gmail.com>
2022-02-17 23:06:30 +0000

    adding winning conditions to PC and player sections. Adding more HTML for text to appear when a choice is selected and score updates. Updates mainly to JS and HTML. styling will be last once all the JS functions are working correctly.

--- assests/scripts/scripts.js
+++ assests/scripts/scripts.js
@@ -1,40 +1,70 @@
-// Finding buttons
-const playerOptions = document.querySelectorAll('.player-gamebtn');
+// Empty strings to hold player and bot choices when selected
+let userChoice = '';
+let botChoice = '';
+
+// empty values to hold the scores
+let playerScore = 0;
+let pcScore = 0;
+
+// Finding classes to update the scores
+const updateScore = () => {
+  const userScore = document.querySelector('.player-score h3');
+  const botScore = document.querySelector('.pc-score h3');
+  userScore.textContent = playerScore;
+  botScore.textContent = pcScore;
+};
+
+//finding winner class
+let playerWinner = document.querySelector('.player-winner');
+let pcWinner = document.querySelector('.pc-score');
+
+// finding score elements to add points 
+
+// Finding button
+const playerOptions = document.querySelectorAll(".player-gamebtn");
 // // Loop to get all of the player button classes and find the selection with when clicked
 
+function generateBotOption() {
+  const pcPick = document.getElementsByClassName("bot-gamebtn");
+  const pcOption = pcPick[Math.floor(Math.random() * pcPick.length)];
+  console.log(pcOption.textContent);
+  botChoice = pcOption.textContent;
+}
 
-function game() {
-  // getting the player button input and choice
+function bindUserInput() {
   for (let i = 0; i < playerOptions.length; i++) {
-    playerOptions[i].addEventListener('click', function () {
+    playerOptions[i].addEventListener("click", function () {
       const playerPick = this.innerText;
       console.log(playerPick);
+      userChoice = playerPick;
 
-      ///PC choice
-      const pcPick = document.getElementsByClassName('player-gamebtn');
-      const pcOption = pcPick[Math.floor(Math.random() * pcPick.length)];
-      console.log(pcOption.textContent)
+      // Bot option
+      generateBotOption();
+    
+      // Winners
+      winCondtion()
 
+      // updating scores
+      updateScore();
+    });
+  }
+}
 
-      // winning conditions 
-      if (playerPick === pcOption.textContent) {
-        console.log('draw')
-        return;
-      }
-      if (playerPick === 'sci') {
-        if (pcOption.textContent === 'paper')
-          console.log('You win!')
-          return;
-      } else {
-        console.log('PC won..')
-        return;
-      }
-    })
+function winCondtion(){
+  if(userChoice === botChoice){
+    playerWinner.textContent = 'It is a draw!'
+    return;
+  }
+  if(userChoice === 'rock'){
+    if (botChoice === 'sci') {
+      playerWinner.textContent = 'You win!!'
+      playerScore++;
+      return;
+    } else {
+      pcScore.textContent = 'The PC wins'
+    }
   }
 };
 
-// Printing the choices to the console. 
-function choices() {
-  console.log(game())
-}
-choices()
+
+window.addEventListener('load', bindUserInput);
